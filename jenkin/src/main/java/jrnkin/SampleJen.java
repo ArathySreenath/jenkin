5de73964504a7651bfed0a8a251c81fb8c1d8956
arath <arath@LAPTOP-GKHRCD72>
2023-05-05 18:38:41 +0530

    commit 3

--- jenkin/src/main/java/jrnkin/SampleJen.java
+++ jenkin/src/main/java/jrnkin/SampleJen.java
@@ -6,6 +6,7 @@ public class SampleJen {
 		System.out.println("hello jenkins");
 		System.out.println("commit");
 		System.out.println("jenkin build");
+		System.out.println("Automayic");
 		// TODO Auto-generated method stub
 
 	}
