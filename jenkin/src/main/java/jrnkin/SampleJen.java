e6178853523bfe8cbaee6c3983e44d892efd7866
arath <arath@LAPTOP-GKHRCD72>
2023-05-05 17:39:04 +0530

    2nd commit

--- jenkin/src/main/java/jrnkin/SampleJen.java
+++ jenkin/src/main/java/jrnkin/SampleJen.java
@@ -5,6 +5,7 @@ public class SampleJen {
 	public static void main(String[] args) {
 		System.out.println("hello jenkins");
 		System.out.println("commit");
+		System.out.println("jenkin build");
 		// TODO Auto-generated method stub
 
 	}
